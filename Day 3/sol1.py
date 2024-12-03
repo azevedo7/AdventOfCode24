2084d7bebd824c08bac69e958a19bf732acb78b2
azevedo7 <jazevedo0406@gmail.com>
2024-12-03 12:38:58 +0000

    Day 3 - Part 1

---
+++ Day 3/sol1.py
@@ -0,0 +1,14 @@
+import re
+
+with open("input.txt") as f:
+    text = f.read()
+
+
+results = re.findall("mul\([0-9]{1,3},[0-9]{1,3}\)", text)
+
+total = 0
+for result in results:
+    a,b = map(int, result[4:-1].split(","))
+    total += a * b
+
+print(total)
